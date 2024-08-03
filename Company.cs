26d3f9e57d9167dd2f71252965d8d45950dae402
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-08-03 19:36:51 +0300

    Add files via upload

---
+++ Company.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionPractice
+{
+    public abstract class Company
+    {
+        //Her çalışan için ad, soyad, departman gibi ortak özellikler bulunmaktadır
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Department { get; set; }
+
+        //Ancak her çalışanın görevi (pozisyonu)
+        //farklı olabilir (Örneğin, Yazılım Geliştirici, Proje Yöneticisi, Satış Temsilcisi).
+        public virtual string? title => "Line Collegue";
+
+        //Gorev metodunu çağırdığımızda her çalışan kendi yaptığı işi konsol ekranında söyleyecek.
+        public abstract void Mission();
+
+    }
+
+    public class SoftWareDeveloper : Company
+    {
+        public override string? title => "Software Developer";
+
+        public override void Mission()
+        {
+            Console.WriteLine($" I work as a {title} ");
+        }
+
+
+    }
+
+
+    public class ProjectManager : Company
+    {
+        public override string? title => "Project Manager";
+
+        public override void Mission()
+        {
+            Console.WriteLine($" I work as a {title} ");
+        }
+
+
+    }
+
+
+
+    public class Salesman: Company
+    {
+
+        public override string? title => "Salesman";
+
+        public override void Mission()
+        {
+            Console.WriteLine($" I work as a {title} ");
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using AbstractionPractice;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Company salesman = new Salesman();
+        salesman.Mission();
+
+        Console.WriteLine("---------------------");
+
+
+        Company projectmanager = new ProjectManager();
+        projectmanager.Mission();
+
+        Console.WriteLine("----------------");
+
+        Company softwaredev = new SoftWareDeveloper();
+        softwaredev.Mission();
+
+
+    }
+
+
+
+
+}
